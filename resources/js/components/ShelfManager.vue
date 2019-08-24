9e4bc81fcdb71c4609cc2f0336bfee98ece5f89e
ddukki <ddukki@gmail.com>
2019-08-24 11:45:06 -0500

    Updating css

---
+++ resources/js/components/ShelfManager.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-3 text-center">
+				<div class="card">
+					<div class="card-body">
+						Add New Book<br/>
+						<h1>+</h1>
+					</div>
+				</div>
+			</div>
+			<div v-for="shelf in shelves" class="col-3">
+
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			shelves: [],
+		}
+	},
+	mounted: {
+
+	},
+	methods: {
+
+	},
+}
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,16 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-offset-2 col-md-8">
-			<form class="form">
+			<form class="form" method="POST" action="">
 				@csrf
 				<div class="form-group">
 					<div class="input-group mb-3">
 						<input type="text" class="form-control"
 								placeholder="Search Books"
-								aria-label="Recipient's username"
+								aria-label="Search for books"
 								aria-describedby="basic-addon2">
 						<div class="input-group-append">
-							<button class="btn btn-outline-secondary" type="button">
+							<button class="btn btn-outline-secondary" type="submit">
 								<i class="fa fa-search"></i>
 							</button>
 						</div>
