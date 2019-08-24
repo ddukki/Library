48699c499d01eb2681acfa62e38d13e2c0070fb5
ddukki <ddukki@gmail.com>
2019-08-24 11:27:22 -0500

    Adding font-awesome

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,21 +3,27 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+        <div class="col-offset-2 col-md-8">
+			<form class="form">
+				@csrf
+				<div class="form-group">
+					<div class="input-group mb-3">
+						<input type="text" class="form-control"
+								placeholder="Search Books"
+								aria-label="Recipient's username"
+								aria-describedby="basic-addon2">
+						<div class="input-group-append">
+							<button class="btn btn-outline-secondary" type="button">
+								<i class="fa fa-search"></i>
+							</button>
+						</div>
+					</div>
+				</div>
+			</form>
         </div>
+
+		<shelf-manager>
+		</shelf-manager>
     </div>
 </div>
 @endsection
